66710f4aacb547191674699b375d274191d2ce68
KNR1997 <kethakaranasinghe@gmail.com>
2025-12-01 20:43:36 +0530

    feat(serializer): add subject, teacher

--- course/serializers.py
+++ course/serializers.py
@@ -1,19 +1,19 @@
 from core.serializers.base import BaseSerializer
 from course.models import Course
 from subject.serializers import SubjectListSerializer
-from user.serializers import UserListSerializer
+from user.serializers import TeacherListSerializer
 
 
 class CourseSerializer(BaseSerializer):
+    subject = SubjectListSerializer()
+    teacher = TeacherListSerializer()
+
     class Meta:
         model = Course
         fields = '__all__'
 
 
 class CourseListSerializer(BaseSerializer):
-    subject = SubjectListSerializer()
-    teacher = UserListSerializer()
-
     class Meta:
         model = Course
         fields = '__all__'
