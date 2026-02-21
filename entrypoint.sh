d6272fd48fe7f52b1e8f44c99ea3c8054df8d0d9
KNR1997 <kethakaranasinghe@gmail.com>
2026-02-21 21:16:15 +0530

    feat: entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ echo "Running database migrations..."
 python manage.py migrate --noinput
 
 echo "Starting server..."
-exec gunicorn beyond_health.wsgi:application --bind 0.0.0.0:${PORT:-8000}
+exec gunicorn academy.wsgi:application --bind 0.0.0.0:${PORT:-8000}
