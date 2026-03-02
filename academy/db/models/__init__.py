392b85cf400eafc854c32dbff337fdda5e899a88
KNR1997 <kethakaranasinghe@gmail.com>
2026-03-02 14:20:59 +0530

    migration: Notification, UserNotification

--- academy/db/models/__init__.py
+++ academy/db/models/__init__.py
@@ -4,6 +4,7 @@ from .course_offering import CourseOffering
 from .enrollment import Enrollment
 from .enrollment_payment import EnrollmentPayment
 from .grade_level import GradeLevel
+from .notification import Notification, UserNotification
 from .student import Student
 from .subject import Subject
 from .teacher import Teacher
